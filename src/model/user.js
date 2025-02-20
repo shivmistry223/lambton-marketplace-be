996f75cb82543b9a5d531d6da4aee68e54e1dd28
Shiv Mistry <shivmistry223@gmail.com>
2025-02-20 15:42:47 -0500

    Integreated Registration api

--- src/model/user.js
+++ src/model/user.js
@@ -1,54 +1,63 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password can not contain password");
-      }
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password can not contain password");
+        }
+      },
     },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    courseCode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    termNo: {
+      type: Number,
+      required: true,
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  phoneNumber: {
-    type: Number,
-    required: true,
-  },
-  courseCode: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  termNo: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.toJSON = function () {
   const user = this;
@@ -57,6 +66,43 @@ userSchema.methods.toJSON = function () {
   delete userObject.password;
   return userObject;
 };
+
+userSchema.methods.getAuthToken = async function () {
+  const user = this;
+
+  user.tokens;
+
+  const token = jwt.sign({ _id: user._id.toString() }, "will2025");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (userName, password) => {
+  const user = await User.findOne({ userName });
+
+  if (!user) {
+    throw new Error("User Not Found");
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) {
+    throw new Error("Invalid Password");
+  }
+
+  return user;
+};
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
